e84d59f9b618bcdc0a91ca2a27b3c79c4f17315f
Mpumelelo Msane <160489637+mpumelelomsane@users.noreply.github.com>
2026-08-06 22:37:44 +0000

    Learning while loops on CS50

---
+++ CS50_Loops.py
@@ -0,0 +1,24 @@
+# LOOPS
+# Essentially ,loops are a way of doing something over and over again 
+# instead of printing the same stamement over and over again, we use WHILE loops 
+# to create a block of code that executes over & over
+
+i= 3
+while i != 0 :
+    print("Meow")     
+    i= i-1 
+"""while loops work by repeatedly asking if the
+condition of the loop has been ful�lled. In this case, the interpreter is asking, “does i
+not equal zero?"""  
+ 
+#EXAMPLE2  
+i=1
+while i <= 3 :
+    print ("Hi Mpumey!")
+    i =i+1 
+
+"""Notice that when we code i = i + 1 we assign the value of i from the right to the
+left. Above, we are starting i at one, like most humans count (1, 2, 3). If you execute
+the code above, you’ll see it meows three times. It’s best practice in programming to
+begin counting with zero"""
+
